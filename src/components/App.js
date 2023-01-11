7bc0c33276e7c8bdb82945f13ccb5829fd5f1387
robinbenitezmora <robinbenitez1990@gmail.com>
2023-01-10 23:14:58 -0500

    Correct linters errors version5

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import store from '../redux/store';
 import Greeting from './Greeting';
+import store from '../redux/store';
 
 function App() {
   return (
-    /*eslint-disable */
     <Provider store={store}>
       <Router>
         <Routes>
